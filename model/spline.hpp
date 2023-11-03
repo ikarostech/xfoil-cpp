601870bc1c1e3337283b984351f815a9f6fcb5c8
ikarostech <ikarostech@live.jp>
2023-11-04 05:08:54 +0900

    fix-commentout

--- model/spline.hpp
+++ model/spline.hpp
@@ -11,7 +11,6 @@ class spline {
      * @param n @deprecated データ数
      * @return std::vector<double> 始点からの累計長さのデータ
      */
-    //static std::vector<double> scalc(const double x[], const double y[], int n);
     static std::vector<double> scalc(const double x[], const double y[], int n, const int s_size);
     static double seval(double ss, const double x[], const double xs[], const double s[], int n);
 };
