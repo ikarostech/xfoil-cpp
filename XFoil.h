169b67bae304fe95f6a29acb11fe4eac8ec9b277
ikarostech <ikarostech@live.jp>
2024-04-29 20:53:09 +0900

    boundary_layerにクラスを移植

--- XFoil.h
+++ XFoil.h
@@ -49,46 +49,12 @@ Harold Youngren. See http://raphael.mit.edu/xfoil for more information.
 
 #include "model/matrix.hpp"
 #include "model/spline.hpp"
+#include "model/boundary_layer.hpp"
 #include "xfoil_params.h"
 
 using namespace std;
 using namespace Eigen;
 //------ derived dimensioning limit parameters
-
-class blData {
-  public:
-  class blVector {
-  public:
-    double scalar;
-    Vector<double, 6> vector = Vector<double, 6>::Zero();
-    Vector<double, 3> pos_vector() {
-      return vector.segment(0, 3);
-    }
-    double& u() {
-      return vector[0];
-    }
-    double& t() {
-      return vector[1];
-    }
-    double& d() {
-      return vector[2];
-    }
-    double& s() {
-      return vector[3];
-    }
-    double& ms() {
-      return vector[4];
-    }
-    double& re() {
-      return vector[5];
-    }
-  };
-  blVector hkz, hsz, hcz, rtz, cfz, diz, usz, cqz, dez;
-  double xz, uz, tz, dz, sz, amplz, uz_uei, uz_ms, dwz, 
-      hz, hz_tz, hz_dz, 
-      mz, mz_uz, mz_ms, 
-      rz, rz_uz, rz_ms;
-};
 
 class XFoil {
  public:
