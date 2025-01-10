b6931ef8c389e4c06c6a5bd3d1c7d27dd2595289
ikarostech <ikarostech@live.jp>
2025-01-11 03:05:50 +0900

    処理速度に影響ない範囲でのベクトル化

--- XFoil.cpp
+++ XFoil.cpp
@@ -690,16 +690,16 @@ bool XFoil::bldif(int ityp) {
       z_hk2 = upw * z_hka;
 
       vs1(0, 0) = z_s1;
-      vs1(0, 1) = z_de * blData1.dez.t() + z_us * blData1.usz.t();
-      vs1(0, 2) = z_d + z_de * blData1.dez.d() + z_us * blData1.usz.d();
-      vs1(0, 3) = z_u1 + z_de * blData1.dez.u() + z_us * blData1.usz.u();
-      vs1.col(0).segment(1,3) += z_upw * upw1;
+      //vs1(0, 1) = z_us * blData1.usz.t();
+      vs1(0, 2) = z_d;// + z_us * blData1.usz.d();
+      vs1(0, 3) = z_u1;// + z_us * blData1.usz.u();
+      vs1.col(0).segment(1,3) += (z_upw * upw1) + (z_de * blData1.dez.pos_vector()) + (z_us * blData1.usz.pos_vector());
       vs1(0, 4) = z_x1;
       vs2(0, 0) = z_s2;
-      vs2(0, 1) = z_de * blData2.dez.t() + z_us * blData2.usz.t();
-      vs2(0, 2) = z_d + z_de * blData2.dez.d() + z_us * blData2.usz.d();
-      vs2(0, 3) = z_u2 + z_de * blData2.dez.u() + z_us * blData2.usz.u();
-      vs2.col(0).segment(1,3) += z_upw * upw2;
+      //vs2(0, 1) = z_de * blData2.dez.t() + z_us * blData2.usz.t();
+      vs2(0, 2) = z_d;// + z_de * blData2.dez.d() + z_us * blData2.usz.d();
+      vs2(0, 3) = z_u2;// + z_de * blData2.dez.u() + z_us * blData2.usz.u();
+      vs2.col(0).segment(1,3) += (z_upw * upw2) + (z_de * blData2.dez.pos_vector()) + (z_us * blData2.usz.pos_vector());
       vs2(0, 4) = z_x2;
       vsm[0] = z_upw * upw_ms + z_de * blData1.dez.ms() + z_us * blData1.usz.ms() +
                z_de * blData2.dez.ms() + z_us * blData2.usz.ms();
@@ -707,6 +707,8 @@ bool XFoil::bldif(int ityp) {
       vs1(0, 1) += z_cq1 * blData1.cqz.t() + z_cf1 * blData1.cfz.t() + z_hk1 * blData1.hkz.t();
       vs1(0, 2) += z_cq1 * blData1.cqz.d() + z_cf1 * blData1.cfz.d() + z_hk1 * blData1.hkz.d();
       vs1(0, 3) += z_cq1 * blData1.cqz.u() + z_cf1 * blData1.cfz.u() + z_hk1 * blData1.hkz.u();
+      // ↓ とんでもなく遅いので変数の方を変えるとかしないと無理そう
+      //vs1.col(0).segment(1,3) += (z_cq1 * blData1.cqz.pos_vector()) + (z_cf1 * blData1.cfz.pos_vector()) + (z_hk1 * blData1.hkz.pos_vector());
 
       vs2(0, 1) += z_cq2 * blData2.cqz.t() + z_cf2 * blData2.cfz.t() + z_hk2 * blData2.hkz.t();
       vs2(0, 2) += z_cq2 * blData2.cqz.d() + z_cf2 * blData2.cfz.d() + z_hk2 * blData2.hkz.d();
