7d689f239761997f214e7d6e50df359e776524a0
ikarostech <ikarostech@live.jp>
2025-12-22 03:18:42 +0900

    blsolveを純粋関数化1

--- src/simulation/XFoil_blsolve.cpp
+++ src/simulation/XFoil_blsolve.cpp
@@ -4,6 +4,9 @@
 
 #include "XFoil.h"
 
+#include <array>
+#include <vector>
+
 namespace {
 inline void plu3x3(double m[3][3], int piv[3]) {
   piv[0] = 0;
@@ -65,21 +68,42 @@ inline void luSolve3x3x6(const double m[3][3], const int piv[3],
 }
 } // namespace
 
-bool XFoil::blsolve() {
+namespace {
+using VmVector = std::vector<double>;
+using VzArray = std::array<std::array<double, 2>, 3>;
 
+struct BlSolveOutput {
+  VmVector vm;
+  XFoil::Matrix3x2dVector vdel;
+};
+
+inline int vmIndex(int k, int i, int j) {
+  return (k * IZX + i) * IZX + j;
+}
+
+BlSolveOutput blsolvePure(
+    int nsys,
+    int ivte1,
+    int ivz,
+    double vaccel,
+    const XFoil::Matrix3x2dVector& va,
+    const XFoil::Matrix3x2dVector& vb,
+    VmVector vm,
+    XFoil::Matrix3x2dVector vdel,
+    const VzArray& vz) {
   auto eliminateVaBlock = [&](int iv, int ivp) {
-    double D[3][3] = {{va[iv](0, 0), va[iv](0, 1), vm[0][iv][iv]},
-                      {va[iv](1, 0), va[iv](1, 1), vm[1][iv][iv]},
-                      {va[iv](2, 0), va[iv](2, 1), vm[2][iv][iv]}};
+    double D[3][3] = {{va[iv](0, 0), va[iv](0, 1), vm[vmIndex(0, iv, iv)]},
+                      {va[iv](1, 0), va[iv](1, 1), vm[vmIndex(1, iv, iv)]},
+                      {va[iv](2, 0), va[iv](2, 1), vm[vmIndex(2, iv, iv)]}};
     int piv[3];
     plu3x3(D, piv);
 
     double rhs[3][6] = {};
     int cols = 0;
     for (int offset = 0; offset < 3 && iv + offset <= nsys; ++offset, ++cols) {
-      rhs[0][cols] = vm[0][iv + offset][iv];
-      rhs[1][cols] = vm[1][iv + offset][iv];
-      rhs[2][cols] = vm[2][iv + offset][iv];
+      rhs[0][cols] = vm[vmIndex(0, iv + offset, iv)];
+      rhs[1][cols] = vm[vmIndex(1, iv + offset, iv)];
+      rhs[2][cols] = vm[vmIndex(2, iv + offset, iv)];
     }
     rhs[0][cols] = vdel[iv](0, 0);
     rhs[1][cols] = vdel[iv](1, 0);
@@ -93,9 +117,9 @@ bool XFoil::blsolve() {
 
     int idx = 0;
     for (int offset = 0; offset < 3 && iv + offset <= nsys; ++offset, ++idx) {
-      vm[0][iv + offset][iv] = rhs[0][idx];
-      vm[1][iv + offset][iv] = rhs[1][idx];
-      vm[2][iv + offset][iv] = rhs[2][idx];
+      vm[vmIndex(0, iv + offset, iv)] = rhs[0][idx];
+      vm[vmIndex(1, iv + offset, iv)] = rhs[1][idx];
+      vm[vmIndex(2, iv + offset, iv)] = rhs[2][idx];
     }
     vdel[iv](0, 0) = rhs[0][idx];
     vdel[iv](1, 0) = rhs[1][idx];
@@ -106,26 +130,28 @@ bool XFoil::blsolve() {
     vdel[iv](2, 1) = rhs[2][idx];
 
     for (int l = iv + 3; l <= nsys; ++l) {
-      double col[3] = {vm[0][l][iv], vm[1][l][iv], vm[2][l][iv]};
+      double col[3] = {vm[vmIndex(0, l, iv)], vm[vmIndex(1, l, iv)],
+                       vm[vmIndex(2, l, iv)]};
       luSolve3x3(D, piv, col);
-      vm[0][l][iv] = col[0];
-      vm[1][l][iv] = col[1];
-      vm[2][l][iv] = col[2];
+      vm[vmIndex(0, l, iv)] = col[0];
+      vm[vmIndex(1, l, iv)] = col[1];
+      vm[vmIndex(2, l, iv)] = col[2];
     }
   };
 
-  auto eliminateVbBlock = [&](int iv, int ivp, int ivte1) {
-    double D[3][3] = {{vb[ivp](0, 0), vb[ivp](0, 1), vm[0][iv][ivp]},
-                      {vb[ivp](1, 0), vb[ivp](1, 1), vm[1][iv][ivp]},
-                      {vb[ivp](2, 0), vb[ivp](2, 1), vm[2][iv][ivp]}};
+  auto eliminateVbBlock = [&](int iv, int ivp) {
+    double D[3][3] = {{vb[ivp](0, 0), vb[ivp](0, 1), vm[vmIndex(0, iv, ivp)]},
+                      {vb[ivp](1, 0), vb[ivp](1, 1), vm[vmIndex(1, iv, ivp)]},
+                      {vb[ivp](2, 0), vb[ivp](2, 1), vm[vmIndex(2, iv, ivp)]}};
 
     double col[3];
     for (int l = ivp; l <= nsys; ++l) {
-      col[0] = vm[0][l][iv];
-      col[1] = vm[1][l][iv];
-      col[2] = vm[2][l][iv];
+      col[0] = vm[vmIndex(0, l, iv)];
+      col[1] = vm[vmIndex(1, l, iv)];
+      col[2] = vm[vmIndex(2, l, iv)];
       for (int k = 0; k < 3; ++k)
-        vm[k][l][ivp] -= D[k][0] * col[0] + D[k][1] * col[1] + D[k][2] * col[2];
+        vm[vmIndex(k, l, ivp)] -=
+            D[k][0] * col[0] + D[k][1] * col[1] + D[k][2] * col[2];
     }
 
     col[0] = vdel[iv](0, 0);
@@ -141,16 +167,15 @@ bool XFoil::blsolve() {
       vdel[ivp](k, 1) -= D[k][0] * col[0] + D[k][1] * col[1] + D[k][2] * col[2];
 
     if (iv == ivte1) {
-      int ivz = boundaryLayerWorkflow.lattice.bottom.stationToSystem[boundaryLayerWorkflow.lattice.bottom.trailingEdgeIndex];
       double Dz[3][2] = {{vz[0][0], vz[0][1]},
                          {vz[1][0], vz[1][1]},
                          {vz[2][0], vz[2][1]}};
 
       for (int l = ivp; l <= nsys; ++l) {
-        col[0] = vm[0][l][iv];
-        col[1] = vm[1][l][iv];
+        col[0] = vm[vmIndex(0, l, iv)];
+        col[1] = vm[vmIndex(1, l, iv)];
         for (int k = 0; k < 3; ++k)
-          vm[k][l][ivz] -= Dz[k][0] * col[0] + Dz[k][1] * col[1];
+          vm[vmIndex(k, l, ivz)] -= Dz[k][0] * col[0] + Dz[k][1] * col[1];
       }
 
       col[0] = vdel[iv](0, 0);
@@ -167,24 +192,24 @@ bool XFoil::blsolve() {
 
   auto eliminateLowerVmColumn = [&](int iv, int ivp) {
     for (int kv = iv + 2; kv <= nsys; kv++) {
-      double vtmp1 = vm[0][iv][kv];
-      double vtmp2 = vm[1][iv][kv];
-      double vtmp3 = vm[2][iv][kv];
-      if (fabs(vtmp1) > VAccel()) {
+      double vtmp1 = vm[vmIndex(0, iv, kv)];
+      double vtmp2 = vm[vmIndex(1, iv, kv)];
+      double vtmp3 = vm[vmIndex(2, iv, kv)];
+      if (fabs(vtmp1) > vaccel) {
         for (int l = ivp; l <= nsys; l++)
-          vm[0][l][kv] -= vtmp1 * vm[2][l][iv];
+          vm[vmIndex(0, l, kv)] -= vtmp1 * vm[vmIndex(2, l, iv)];
         vdel[kv](0, 0) -= vtmp1 * vdel[iv](2, 0);
         vdel[kv](0, 1) -= vtmp1 * vdel[iv](2, 1);
       }
-      if (fabs(vtmp2) > VAccel()) {
+      if (fabs(vtmp2) > vaccel) {
         for (int l = ivp; l <= nsys; l++)
-          vm[1][l][kv] -= vtmp2 * vm[2][l][iv];
+          vm[vmIndex(1, l, kv)] -= vtmp2 * vm[vmIndex(2, l, iv)];
         vdel[kv](1, 0) -= vtmp2 * vdel[iv](2, 0);
         vdel[kv](1, 1) -= vtmp2 * vdel[iv](2, 1);
       }
-      if (fabs(vtmp3) > VAccel()) {
+      if (fabs(vtmp3) > vaccel) {
         for (int l = ivp; l <= nsys; l++)
-          vm[2][l][kv] -= vtmp3 * vm[2][l][iv];
+          vm[vmIndex(2, l, kv)] -= vtmp3 * vm[vmIndex(2, l, iv)];
         vdel[kv](2, 0) -= vtmp3 * vdel[iv](2, 0);
         vdel[kv](2, 1) -= vtmp3 * vdel[iv](2, 1);
       }
@@ -195,30 +220,61 @@ bool XFoil::blsolve() {
     for (int iv = nsys; iv >= 2; iv--) {
       double vtmp = vdel[iv](2, 0);
       for (int kv = iv - 1; kv >= 1; kv--) {
-        vdel[kv](0, 0) -= vm[0][iv][kv] * vtmp;
-        vdel[kv](1, 0) -= vm[1][iv][kv] * vtmp;
-        vdel[kv](2, 0) -= vm[2][iv][kv] * vtmp;
+        vdel[kv](0, 0) -= vm[vmIndex(0, iv, kv)] * vtmp;
+        vdel[kv](1, 0) -= vm[vmIndex(1, iv, kv)] * vtmp;
+        vdel[kv](2, 0) -= vm[vmIndex(2, iv, kv)] * vtmp;
       }
       vtmp = vdel[iv](2, 1);
       for (int kv = iv - 1; kv >= 1; kv--) {
-        vdel[kv](0, 1) -= vm[0][iv][kv] * vtmp;
-        vdel[kv](1, 1) -= vm[1][iv][kv] * vtmp;
-        vdel[kv](2, 1) -= vm[2][iv][kv] * vtmp;
+        vdel[kv](0, 1) -= vm[vmIndex(0, iv, kv)] * vtmp;
+        vdel[kv](1, 1) -= vm[vmIndex(1, iv, kv)] * vtmp;
+        vdel[kv](2, 1) -= vm[vmIndex(2, iv, kv)] * vtmp;
       }
     }
   };
 
-  int ivte1 = boundaryLayerWorkflow.lattice.top.stationToSystem[boundaryLayerWorkflow.lattice.top.trailingEdgeIndex];
   for (int iv = 1; iv <= nsys; iv++) {
     int ivp = iv + 1;
     eliminateVaBlock(iv, ivp);
     if (iv != nsys) {
-      eliminateVbBlock(iv, ivp, ivte1);
+      eliminateVbBlock(iv, ivp);
       if (ivp != nsys)
         eliminateLowerVmColumn(iv, ivp);
     }
   }
 
   backSubstitute();
+  return {std::move(vm), std::move(vdel)};
+}
+} // namespace
+
+bool XFoil::blsolve() {
+  VmVector vm_input(3 * IZX * IZX, 0.0);
+  for (int k = 0; k < 3; ++k) {
+    for (int i = 0; i < IZX; ++i) {
+      for (int j = 0; j < IZX; ++j) {
+        vm_input[vmIndex(k, i, j)] = vm[k][i][j];
+      }
+    }
+  }
+  VzArray vz_input{};
+  for (int k = 0; k < 3; ++k) {
+    for (int j = 0; j < 2; ++j) {
+      vz_input[k][j] = vz[k][j];
+    }
+  }
+
+  int ivte1 = boundaryLayerWorkflow.lattice.top.stationToSystem[boundaryLayerWorkflow.lattice.top.trailingEdgeIndex];
+  int ivz = boundaryLayerWorkflow.lattice.bottom.stationToSystem[boundaryLayerWorkflow.lattice.bottom.trailingEdgeIndex];
+
+  auto result = blsolvePure(nsys, ivte1, ivz, VAccel(), va, vb, vm_input, vdel, vz_input);
+  for (int k = 0; k < 3; ++k) {
+    for (int i = 0; i < IZX; ++i) {
+      for (int j = 0; j < IZX; ++j) {
+        vm[k][i][j] = result.vm[vmIndex(k, i, j)];
+      }
+    }
+  }
+  vdel = std::move(result.vdel);
   return true;
 }
